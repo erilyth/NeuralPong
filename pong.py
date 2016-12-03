9a22732b18e1782f144f407cac5db6fe3549a1fc
erilyth <vishal.batchu@students.iiit.ac.in>
2016-12-03 07:34:16 +0530

    Add ball and paddles

--- pong.py
+++ pong.py
@@ -6,18 +6,20 @@ from pygame.locals import *
 screen_width = 800
 screen_height = 600
 
-paddle_length = 100
+paddle_length = 120
 paddle_width = 20
-player1_pos = 0 # Changes from 0 to screen_height-paddle_length
-player2_pos = 0 # Changes from 0 to screen_height-paddle_length
+
+player1_pos = [0, 0] # Changes from 0 to screen_height-paddle_length
+player2_pos = [screen_width-paddle_width, 0] # Changes from 0 to screen_height-paddle_length
+ball_pos = [screen_width/2, screen_height/2]
+
+def draw_ball(background):
+	pygame.draw.circle(background, (255, 255, 255), ball_pos, 10)
+	return background
 
 def draw_paddles(background):
-	bar1 = pygame.Surface((paddle_width, paddle_length))
-	bar2 = pygame.Surface((paddle_width, paddle_length))
-	bar1.fill((255, 255, 255))
-	bar2.fill((255, 255, 255))
-	background.blit(bar1, (0, player1_pos))
-	background.blit(bar2, (screen_width-paddle_width, player2_pos))
+	pygame.draw.rect(background, (255, 255, 255), (player1_pos[0], player1_pos[1], paddle_width, paddle_length))
+	pygame.draw.rect(background, (255, 255, 255), (player2_pos[0], player2_pos[1], paddle_width, paddle_length))
 	return background
 
 def main():
@@ -36,7 +38,10 @@ def main():
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
+		player1_pos[1] += 1
+		background.fill((0, 0, 0))
 		background_new = draw_paddles(background)
+		background_new = draw_ball(background_new)
 		screen.blit(background_new, (0, 0))
 		pygame.display.flip()
 
