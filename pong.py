81893f53dd84f34c6b8a4a36e15f73d4e35dd609
Batchu Venkat Vishal <vishal.batchu@students.iiit.ac.in>
2016-12-13 17:29:49 +0530

    Modify to make it single player pong Training would be faster this way

--- pong.py
+++ pong.py
@@ -14,6 +14,7 @@ screen_width = 400
 screen_height = 300
 
 paddle_length = 60
+paddle_length2 = 300
 paddle_width = 10
 ball_radius = 5
 paddle_speed = 4
@@ -21,7 +22,6 @@ ball_speed = 2
 max_speed = 5
 
 player1_pos = [0, screen_height/2-paddle_length/2] # Changes from 0, 0 to 0, screen_height-paddle_length-1
-player2_pos = [screen_width-paddle_width, screen_height/2-paddle_length/2] # Changes from 0 to screen_width-paddle_width, 0 to screen_width-paddle_width, screen_height-paddle_length-1
 ball_pos = [screen_width/2, screen_height/2]
 ball_velocity = [0, 0]
 final_img = np.zeros((screen_height, screen_width))
@@ -58,8 +58,7 @@ def retrieve_image(background):
                     final_img[ball_pos[1]+j][ball_pos[0]+i] = 1
     for i in range(paddle_width):
         for j in range(paddle_length):
-            final_img[player1_pos[1]+j][player1_pos[0]+i] = 1
-            final_img[player2_pos[1]+j][player2_pos[0]+i] = 1
+            final_img[int(player1_pos[1])+j][int(player1_pos[0])+i] = 1
     diff_img = final_img - old_img
     # Gives us an RGB image
     # final_img = pygame.surfarray.array3d(pygame.display.get_surface())
@@ -89,66 +88,77 @@ def check_collision():
     global ball_pos
     global ball_velocity
     global player1_pos
-    global player2_pos
     rect1 = Rect(player1_pos[0], player1_pos[1], paddle_width, paddle_length)
-    rect2 = Rect(player2_pos[0], player2_pos[1], paddle_width, paddle_length)
     rect3 = Rect(ball_pos[0]-ball_radius, ball_pos[1]-ball_radius, 2*ball_radius, 2*ball_radius)
-    if rect3.colliderect(rect1) or rect3.colliderect(rect2):
+    if rect3.colliderect(rect1):
         return True
     return False
+
+def train_network(status):
+    global total_inputs
+    global total_class_outputs
+    if status == True:
+        # Neural net won
+        for ix in range(len(total_inputs)):
+            cur_inp = np.atleast_2d(total_inputs[ix])
+            act_out = np.atleast_2d(total_class_outputs[ix])
+            model.fit(cur_inp, act_out, nb_epoch=1, batch_size=1)
+    else:
+        # Neural net lost
+        for ix in range(len(total_inputs)):
+            cur_inp = np.atleast_2d(total_inputs[ix])
+            cur_out = [0, 0, 0]
+            for iy in range(3):
+                if total_class_outputs[ix][iy] == 1:
+                    cur_out[iy] = 0
+                else:
+                    cur_out[iy] = 0.5
+            act_out = np.atleast_2d(cur_out)
+            model.fit(cur_inp, act_out, nb_epoch=1, batch_size=1)
+    print("Done updating weights!")
+    # Save the model
+    model.save_weights("model.keras")
+
+def reset_game():
+    global ball_pos
+    global player1_pos
+    global total_class_outputs
+    global total_inputs
+    ball_pos = [screen_width / 2, screen_height / 2]
+    player1_pos = [0, screen_height / 2 - paddle_length / 2]  # Changes from 0, 0 to 0, screen_height-paddle_length-1
+    initialize_ball()
+    total_inputs = []
+    total_class_outputs = []
 
 def draw_ball(background):
     global ball_pos
     global ball_velocity
     global player1_pos
-    global player2_pos
     ball_pos[0] += ball_velocity[0]
     ball_pos[1] += ball_velocity[1]
-    if ball_pos[0] > screen_width - ball_radius - paddle_width/2 or ball_pos[0] < ball_radius + paddle_width/2:
-        global total_inputs
-        global total_class_outputs
-        if ball_pos[0] > screen_width - ball_radius - paddle_width/2:
-            # Neural net won. Perform updates
-            for ix in range(len(total_inputs)):
-                cur_inp = np.atleast_2d(total_inputs[ix])
-                act_out = np.atleast_2d(total_class_outputs[ix])
-                model.fit(cur_inp, act_out, nb_epoch=1, batch_size=1)
-        else:
-            # Neural net lost. Perform updates
-            for ix in range(len(total_inputs)):
-                cur_inp = np.atleast_2d(total_inputs[ix])
-                cur_out = [0, 0, 0]
-                for iy in range(3):
-                    if total_class_outputs[ix][iy] == 1:
-                        cur_out[iy] = 0
-                    else:
-                        cur_out[iy] = 0.5
-                act_out = np.atleast_2d(cur_out)
-                model.fit(cur_inp, act_out, nb_epoch=1, batch_size=1)
-        print("Done updating weights!")
-        # Save the model
-        total_inputs = []
-        total_class_outputs = []
-        model.save_weights("model.keras")
-        ball_pos = [screen_width/2, screen_height/2]
-        player1_pos = [0, screen_height / 2 - paddle_length / 2]  # Changes from 0, 0 to 0, screen_height-paddle_length-1
-        player2_pos = [screen_width - paddle_width, screen_height / 2 - paddle_length / 2]  # Changes from 0 to screen_width-paddle_width, 0 to screen_width-paddle_width, screen_height-paddle_length-1
-        initialize_ball()
+    if ball_pos[0] < ball_radius + paddle_width/2:
+        train_network(False)
+        reset_game()
     if ball_pos[1] > screen_height - ball_radius or ball_pos[1] < ball_radius:
         if ball_pos[1] < ball_radius:
             ball_pos[1] = ball_radius
         else:
             ball_pos[1] = screen_height - ball_radius
         ball_velocity[1] *= -1
+    if ball_pos[0] > screen_width - ball_radius:
+        ball_pos[0] = screen_width - ball_radius;
+        ball_velocity[0] *= -1
     if check_collision():
-        if ball_velocity[0] < 0:
-            gap = abs(ball_pos[1] - (player1_pos[1]+paddle_length/2))*1.0 / (paddle_length / 2)
-        else:
-            gap = abs(ball_pos[1] - (player2_pos[1]+paddle_length/2))*1.0 / (paddle_length / 2)
-        ball_pos[0] -= ball_velocity[0]
-        ball_velocity[0] = int(round(ball_velocity[0]*(-1)*(max(0.8, gap+0.7))))
-        ball_velocity[1] = int(round(ball_velocity[1]*(max(0.9, gap+0.4))))
         print('Collided!')
+        gap = abs(ball_pos[1] - (player1_pos[1]+paddle_length/2))*1.0 / (paddle_length / 2)
+        ball_velocity[0] = int(round(ball_velocity[0] * (-1) * (max(0.8, gap + 0.7))))
+        ball_velocity[1] = int(round(ball_velocity[1] * (max(0.9, gap + 0.4))))
+        ball_pos[0] += ball_velocity[0]
+        train_network(True)
+        global total_inputs
+        global total_class_outputs
+        total_inputs = []
+        total_class_outputs = []
     ball_velocity[0] = max(-max_speed, min(max_speed, ball_velocity[0]))
     ball_velocity[1] = max(-max_speed, min(max_speed, ball_velocity[1]))
     ball_pos[0] = round(ball_pos[0])
@@ -160,16 +170,13 @@ def draw_paddles(background):
     global ball_pos
     global ball_velocity
     global player1_pos
-    global player2_pos
     pygame.draw.rect(background, (255, 255, 255), (player1_pos[0], player1_pos[1], paddle_width, paddle_length))
-    pygame.draw.rect(background, (255, 255, 255), (player2_pos[0], player2_pos[1], paddle_width, paddle_length))
     return background
 
 def main():
     global ball_pos
     global ball_velocity
     global player1_pos
-    global player2_pos
     clock = pygame.time.Clock()
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
@@ -229,25 +236,17 @@ def main():
             player1_pos[1] += paddle_speed
             if player1_pos[1] > screen_height-paddle_length:
                 player1_pos[1] = screen_height-paddle_length
-        """
-        if keys[K_DOWN]:
-            player2_pos[1] += paddle_speed
-            if player2_pos[1] >= screen_height-paddle_length:
-                player2_pos[1] = screen_height-paddle_length-1
-        if keys[K_UP]:
-            player2_pos[1] -= paddle_speed
-            if player2_pos[1] < 0:
-                player2_pos[1] = 0
         if random.uniform(0,1) > 0.5:
-            # True motion
-            if ball_pos[1] < player2_pos[1]+paddle_length/2-paddle_speed:
+            # Automated bot motion
+            if ball_pos[1] < player2_pos[1]+paddle_length2/2-paddle_speed:
                 player2_pos[1] -= paddle_speed
                 if player2_pos[1] < 0:
                     player2_pos[1] = 0
-            elif ball_pos[1] > player2_pos[1]+paddle_length/2+paddle_speed:
+            elif ball_pos[1] > player2_pos[1]+paddle_length2/2+paddle_speed:
                 player2_pos[1] += paddle_speed
-                if player2_pos[1] >= screen_height-paddle_length:
-                    player2_pos[1] = screen_height-paddle_length-1
+                if player2_pos[1] >= screen_height-paddle_length2:
+                    player2_pos[1] = screen_height-paddle_length2-1
+        """
 
 if __name__ == '__main__':
     main()
